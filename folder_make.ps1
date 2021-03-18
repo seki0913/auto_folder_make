b334166d1761df2a60a5130e74435c29eafb82b8
sekiguchi yuzi <tistnmn@gmail.com>
2021-03-18 11:45:32 +0900

    210318 本番運用中

--- folder_make.ps1
+++ folder_make.ps1
@@ -3,37 +3,38 @@ $str_path = (Convert-Path .)
 
 $csv_file = "$($str_path)\folder_data.csv"
 
-$fol_root = "E:\test"
+$fol_root = "E:\"
 
 # 作業開始
 $fol_data = Import-Csv $csv_file -Encoding UTF8
 
 # フォルダ作成
 foreach($fol_check in $fol_data){
-    $fol_check.name
     if(Test-Path "$($fol_root)\$($fol_Check.folder)"){
     }else{
-        New-Item "$($fol_root)\$($fol_Check.folder)" -ItemType Directory
+        $fol = New-Item "$($fol_root)\$($fol_Check.folder)" -ItemType Directory
+
     }
 }
 
 # ファイルのリンクルートを作成
-$file = Get-ChildItem
+$file = Get-ChildItem $fol_root
 
 # 指定した名前が含まえる場合、移動する
 foreach($fol_check in $fol_data){
     foreach($file_check in $file){
-        $file_check.name
-        $fol_check.name
         if(($file_check.name.contains("$($fol_check.name)")) -and ($file_check.name.contains("mp4")) ){
-            Write-Host "いました"
-            Move-Item "$($fol_root)\$($file_check.name)" "$($fol_root)\$($fol_check.name)"
+            if($file_check.name.contains("lnk")){
 
-            # ショートカットリンクを作成する
-            $WshShell = New-Object -ComObject WScript.Shell
-            $ShortCut = $WshShell.CreateShortcut("$($fol_root)\$($file_check.name).lnk")
-            $ShortCut.TargetPath = "$($fol_root)\$($fol_check.name)\$($file_check.name)"
-            $ShortCut.Save()
+            }else{
+                Move-Item "$($fol_root)\$($file_check.name)" "$($fol_root)\$($fol_check.folder)"
+
+                # ショートカットリンクを作成する
+                $WshShell = New-Object -ComObject WScript.Shell
+                $ShortCut = $WshShell.CreateShortcut("$($fol_root)\$($file_check.name).lnk")
+                $ShortCut.TargetPath = "$($fol_root)\$($fol_check.folder)\$($file_check.name)"
+                $ShortCut.Save()
+            }
         }
     }
 }
